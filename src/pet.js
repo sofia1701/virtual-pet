3689dbe1386ccbba75d33b41dd55b96a4a44616a
sofia1701 <60626518+sofia1701@users.noreply.github.com>
2020-03-28 15:57:06 +0000

    Add files via upload

--- src/pet.js
+++ src/pet.js
@@ -22,54 +22,52 @@ class Pet {
     growUp() {
         if(!this.isAlive) {
             throw new Error('Your pet is no longer alive :(');
-        }else{
-            this.age += 1
-            this.hunger += 5;
-            this.fitness -= 3;
-        }   
+        }
+        this.age += 1
+        this.hunger += 5;
+        this.fitness -= 3; 
     };
 
     walk() {
         if(!this.isAlive) {
             throw new Error('Your pet is no longer alive :(');
+        }
+        if((this.fitness + 4) <= MAXIMUM_FITNESS){
+            this.fitness += 4;
         }else{
-            if((this.fitness + 4) <= MAXIMUM_FITNESS){
-                this.fitness += 4;
-            }else{
-                this.fitness = MAXIMUM_FITNESS;     
-            }
+            this.fitness = MAXIMUM_FITNESS;     
         }  
     };
 
     feed() {
         if(!this.isAlive) {
             throw new Error('Your pet is no longer alive :(');
-        } else {
-            if((this.hunger - 3 >= MINIMUM_HUNGER)) {
-                this.hunger -= 3;
-            }else{
-                this.hunger = MINIMUM_HUNGER;
-            }
+        } 
+        if((this.hunger - 3 >= MINIMUM_HUNGER)) {
+            this.hunger -= 3;
+        }else{
+            this.hunger = MINIMUM_HUNGER;
         }   
     };
 
     checkUp() {
         if(!this.isAlive) {
             throw new Error('Your pet is no longer alive :(');
-        } else {
-            if(this.fitness <= 3 && this.hunger >= 5) {
-                return 'I am hungry AND I need a walk!';
-            }else if(this.fitness <= 3) {
-                return 'I need a walk!';
-            }else if(this.hunger >= 5) {
-                return 'I am hungry!';
-            }else{
-                return 'I feel great!';
-            }
-        }   
+        } 
+        if(this.fitness <= 3 && this.hunger >= 5) {
+            return 'I am hungry AND I need a walk!';
+        }else if(this.fitness <= 3) {
+            return 'I need a walk!';
+        }else if(this.hunger >= 5) {
+            return 'I am hungry!';
+        }
+        return 'I feel great!';   
     };
 
     adoptChild(child) {
+        if(!this.isAlive) {
+            throw new Error('Your pet is no longer alive :(');
+        }
         return this.children = [child];
     };
 };
